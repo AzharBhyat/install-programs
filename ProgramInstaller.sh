83abe12b1d7472e4db25cbef0bad69f6e625606b
AzharBhyat <57112010+AzharBhyat@users.noreply.github.com>
2020-11-28 10:43:01 +0200

    Update ProgramInstaller.sh

--- ProgramInstaller.sh
+++ ProgramInstaller.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
-sudo apt-get update  # To get the latest package lists
+sudo apt-get update  
 
 BLUE='\033[0;34m'
 NC='\033[0m'
 
+cd $HOME/Downloads
+
 wget "https://tlauncher.org/jar"
 printf "Unzipping Tlauncher...\n"
-unzip jar.zip -d home/azhar/tlauncher
+unzip jar.zip -d /tlauncher
 
 printf "${BLUE}Installing OpenJdk & OpenJfx....${NC}\n"
 sudo apt-get install openjdk-8-jre -y
 sudo apt-get install openjfx -y
 
-sudo mv /tlauncher/TLauncher-2.72.jar /home/azhar/Desktop/Minecraft.jar
-sudo rm -r home/azhar/tlauncher
+sudo mv /tlauncher/TLauncher-2.72.jar $HOME/Desktop/Minecraft.jar
+sudo rm -r $HOME/Downloads/tlauncher
+sudo rm $HOME/Downloads/jar.zip
 
 printf "-------------------------------------\n"
 
@@ -21,6 +24,7 @@ printf "${BLUE}Installing snapd...${NC}\n"
 sudo rm /etc/apt/preferences.d/nosnap.pref
 sudo apt update
 sudo apt install snapd -y
+sudo snap
 
 printf "${BLUE}Installing VSCode....${NC}\n"
 sudo snap install code --classic -y
@@ -42,4 +46,8 @@ printf "-------------------------------------\n"
 printf "${BLUE}Installing Vim...${NC}\n"
 sudo apt install vim -y
 
-#etc.
+printf "-------------------------------------\n"
+
+printf "${BLUE}Installing Steam...${NC}\n"
+sudo add-apt-repository multiverse
+sudo apt install steam
